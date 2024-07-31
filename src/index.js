664a4f45186a55cc2bbda31e65f29b2542699a25
Kristella <162458236+kvsc27@users.noreply.github.com>
2024-07-31 13:16:35 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -41,7 +41,13 @@ function randomInteger(min, max) {
  */
 function setDelay(difficulty) {
   // TODO: Write your code here.
-  
+  switch (difficulty.toLowerCase()) {
+    case 'easy':
+      return 1500;
+    case 'normal':
+      return 1000;
+    case 'hard':
+      return 856;
 }
 
 /**
