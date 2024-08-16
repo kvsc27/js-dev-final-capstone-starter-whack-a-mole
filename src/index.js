7ff5255f6c4ebb6bf4efe4a6b4e2e7ebec47ce88
Kristella <162458236+kvsc27@users.noreply.github.com>
2024-08-16 10:16:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -138,25 +138,6 @@ function startGame() {
 }
 
 startButton.addEventListener("click", startGame);
-
-const audioHit = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/hit.mp3?raw=true");
-const song = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/molesong.mp3?raw=true");
-
-function playAudio(audioObject) {
-  audioObject.play();
-}
-
-function loopAudio(audioObject) {
-  audioObject.loop = true;
-  playAudio(audioObject);
-}
-
-function stopAudio(audioObject) {
-  audioObject.pause();
-}
-
-function play(){
-  playAudio(song);
 }
 
 
