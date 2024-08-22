3ff525b5263ea32c2c5add6e804f7fc13e64165b
Kristella <162458236+kvsc27@users.noreply.github.com>
2024-08-22 10:05:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
 const score = document.querySelector("#score");
 const timerDisplay = document.querySelector("#timer");
-const audioHit = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/blob/main/assets/hit.mp3");
-const song = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/raw/main/assets/molesong.mp3");
+const audioHit = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/blob/main/assets/hit.mp3?raw=true");
+const song = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/raw/main/assets/molesong.mp3?raw=true");
 
 function playAudio(audioObject) {
   audioObject.play();
