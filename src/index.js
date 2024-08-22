20bfca572c701b24acb8feb1dc5d6c43cdba363d
Kristella <162458236+kvsc27@users.noreply.github.com>
2024-08-22 11:33:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
 const score = document.querySelector("#score");
 const timerDisplay = document.querySelector("#timer");
-const audioHit = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/raw/main/assets/hit.mp3");
+const audioHit = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/blob/main/assets/88539-Human_smack_face_slap-BLASTWAVEFX-26210.wav");
 AudioHit.preload = 'auto';
 const song = new Audio("https://github.com/kvsc27/js-dev-final-capstone-starter-whack-a-mole/raw/main/assets/molesong.mp3");
 
